720f0b55b114ed284cbe84b82202a2384881deb1
Geazzy Zanoni <zanonigb@gmail.com>
2025-12-11 13:59:08 -0300

    Adiciona variável APPLICATION_ROOT na configuração e atualiza links nas templates para usar url_for. Modifica o docker-compose para incluir configuração do Traefik e define o APPLICATION_ROOT.

--- app/config.py
+++ app/config.py
@@ -4,6 +4,7 @@ class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY', 'dev')
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', 'sqlite:///' + os.path.join(os.path.dirname(__file__), '../instance/app.db'))
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    APPLICATION_ROOT = os.environ.get('APPLICATION_ROOT', '/')
 
 class DevelopmentConfig(Config):
     DEBUG = True
